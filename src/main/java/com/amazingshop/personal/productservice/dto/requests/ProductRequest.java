d7c9715036f5e7b1e7f35df077a61ed44a3c1c27
Matvey Shavlov <m.shavloff@gmail.com>
2025-09-12 16:19:47 +0300

    feat: add new ProductRequest dto

---
+++ src/main/java/com/amazingshop/personal/productservice/dto/requests/ProductRequest.java
@@ -0,0 +1,26 @@
+package com.amazingshop.personal.productservice.dto.requests;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 255, message = "Name must be between 3 and 255 characters")
+    private String name;
+
+    @NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.01", inclusive = true, message = "Price must be greater than 0")
+    private BigDecimal price;
+
+    @NotNull(message = "Stock cannot be null")
+    @Min(value = 0, message = "Stock must be at least 0")
+    private Integer stock;
+}
+
