38873dfd1a3b93a90679be515633d9981974a417
Matvey Shavlov <m.shavloff@gmail.com>
2025-09-12 16:05:45 +0300

    style: swap the lombok annotations in GlobalExceptionHandler class

--- src/main/java/com/amazingshop/personal/productservice/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/amazingshop/personal/productservice/controllers/GlobalExceptionHandler.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
