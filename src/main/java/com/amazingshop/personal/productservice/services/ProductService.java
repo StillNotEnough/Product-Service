6fa58051439a6418fe4a3d7125d3035c246c3344
Matvey Shavlov <m.shavloff@gmail.com>
2025-09-12 16:15:10 +0300

    feat: add save method to ProductService

--- src/main/java/com/amazingshop/personal/productservice/services/ProductService.java
+++ src/main/java/com/amazingshop/personal/productservice/services/ProductService.java
@@ -3,25 +3,22 @@ package com.amazingshop.personal.productservice.services;
 import com.amazingshop.personal.productservice.models.Product;
 import com.amazingshop.personal.productservice.repositories.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
-@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepository;
-    private final ModelMapper modelMapper;
 
     @Autowired
-    public ProductService(ProductRepository productRepository, ModelMapper modelMapper) {
+    public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
-        this.modelMapper = modelMapper;
     }
 
     public Product findProductByIdOrThrow(Long productId) {
@@ -31,5 +28,11 @@ public class ProductService {
 
     public List<Product> findAll() {
         return productRepository.findAll();
+    }
+
+    @Transactional
+    public Product save(Product product) {
+        log.info("Saving product: {}", product.getName());
+        return productRepository.save(product);
     }
 }
